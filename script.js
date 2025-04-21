26f4a780f2012c9ddd06e52864a1fc3929179538
Vasanthkumar M <vasanth23402@gmail.com>
2025-04-21 23:48:23 +0530

    Hero section and post card title section completed

---
+++ script.js
@@ -0,0 +1,45 @@
+const array1 = ['Stylish', 'Smart', 'Confident', 'Fashionable']
+const stylishEle = document.getElementById('stylish')
+
+let wordIndex = 0
+
+function typeWord(word, onComplete) {
+    let charIndex = 0
+    stylishEle.innerText = ''
+
+    const typing = setInterval(() => {
+      if (charIndex < word.length) {
+        stylishEle.innerText += word[charIndex]
+        charIndex++
+      } else {
+        clearInterval(typing)
+        setTimeout(() => onComplete(), 1500)
+      }
+    }, 100)
+  }
+
+  function deleteWord(word, onComplete) {
+    let charIndex = word.length
+
+    const deleting = setInterval(() => {
+      if (charIndex > 0) {
+        stylishEle.innerText = word.slice(0, charIndex - 1)
+        charIndex--
+      } else {
+        clearInterval(deleting)
+        setTimeout(() => onComplete(), 300)
+      }
+    }, 80)
+  }
+
+  function loopTyping() {
+    const word = array1[wordIndex];
+    typeWord(word, () => {
+      deleteWord(word, () => {
+        wordIndex = (wordIndex + 1) % array1.length
+        loopTyping()
+      });
+    });
+  }
+
+  loopTyping()
